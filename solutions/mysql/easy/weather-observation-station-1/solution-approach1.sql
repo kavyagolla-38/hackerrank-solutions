d6be24503f0108e2abba91ac8b81ef79f571336d
kavyagolla-38 <23BQ1A1238@vvit.net>
2026-07-31 10:04:11 +0530

    ✅ Weather Observation Station 1 [mysql] — approach1

---
+++ solutions/mysql/easy/weather-observation-station-1/solution-approach1.sql
@@ -0,0 +1,16 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
+-- Problem     Weather Observation Station 1
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-07-31, 10:04 a.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/
+select city,state
+from station;
