660146081d5d86fe036b119f14ad11808771cb08
kavyagolla-38 <23BQ1A1238@vvit.net>
2026-07-31 11:37:43 +0530

    ✅ Employee Names [mysql] — approach1

---
+++ solutions/mysql/easy/name-of-employees/solution-approach1.sql
@@ -0,0 +1,17 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+-- Problem     Employee Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-07-31, 11:37 a.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/
+select name
+from employee
+order by name asc;
