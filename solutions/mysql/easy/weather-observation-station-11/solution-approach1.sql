b147a93cb7c4983fec01cc5e5bed28ca2c6b7fd0
kavyagolla-38 <23BQ1A1238@vvit.net>
2026-07-31 11:16:32 +0530

    ✅ Weather Observation Station 11 [mysql] — approach1

---
+++ solutions/mysql/easy/weather-observation-station-11/solution-approach1.sql
@@ -0,0 +1,32 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-11/problem?isFullScreen=true
+-- Problem     Weather Observation Station 11
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-07-31, 11:16 a.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/
+
+select distinct city
+from station
+where (
+    city not like "a%"
+    and city not like "e%"
+    and city not like "i%"
+    and city not like "o%"
+    and city not like "u%"
+)
+or
+( 
+    city not like "%a"
+    and city not like "%e"
+    and city not like "%i"
+    and city not like "%o"
+    and city not like "%u"
+);
