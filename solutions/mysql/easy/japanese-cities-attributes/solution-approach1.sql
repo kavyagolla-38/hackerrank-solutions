1feb1f537b78634d21bbd359ec3ff896993d897c
kavyagolla-38 <23BQ1A1238@vvit.net>
2026-07-31 09:49:22 +0530

    ✅ Japanese Cities' Attributes [mysql] — approach1

---
+++ solutions/mysql/easy/japanese-cities-attributes/solution-approach1.sql
@@ -0,0 +1,14 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true
+-- Problem     Japanese Cities' Attributes
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-07-31, 09:49 a.m.
+-- ──────────────────────────────────────────────────
+
+SELECT *
+from city
+where countrycode="jpn";
